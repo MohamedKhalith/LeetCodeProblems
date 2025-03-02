721ffdb41892986ea2eec8b6d483b81bd4cdb1fc
Mohamed Khalith <67334448+MohamedKhalith@users.noreply.github.com>
2025-03-02 15:36:26 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (62.06%) - LeetSync

--- 27-remove-element/remove-element.cs
+++ 27-remove-element/remove-element.cs
@@ -1,6 +1,5 @@
 public class Solution {
     public int RemoveElement(int[] nums, int val) {
-        List<int> listValue = new List<int>();
         int k=0;
         for (int i = 0; i < nums.Length; i++)
         {
